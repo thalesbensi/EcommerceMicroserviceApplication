e64d6614eb8a9fadfc8f5a4cddd60106d4b4f365
thalesbensi <thalesbensi@outlook.com>
2025-12-02 19:20:04 -0300

    Add ProductController and ProductService for product management

---
+++ inventory-ms/src/main/java/com/thalesbensi/inventory_ms/api/controllers/ProductController.java
@@ -0,0 +1,56 @@
+package com.thalesbensi.inventory_ms.api.controllers;
+
+import com.thalesbensi.inventory_ms.api.dtos.ProductRequestDTO;
+import com.thalesbensi.inventory_ms.api.dtos.ProductResponseDTO;
+import com.thalesbensi.inventory_ms.domain.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(@Valid ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ProductResponseDTO>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> findById(@Valid @PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.findById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductResponseDTO> create(@Valid ProductRequestDTO productRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(productRequestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> update(@Valid @PathVariable Long id, ProductRequestDTO productRequestDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.update(id, productRequestDTO));
+    }
+
+    @PostMapping("/validate-stock/{id}")
+    public ResponseEntity<Void> validateStock(@Valid @PathVariable Long id, Integer quantity) {
+        productService.validateStock(id, quantity);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@Valid @PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
---
+++ inventory-ms/src/main/java/com/thalesbensi/inventory_ms/domain/services/ProductService.java
@@ -0,0 +1,83 @@
+package com.thalesbensi.inventory_ms.domain.services;
+
+import com.thalesbensi.inventory_ms.api.dtos.ProductRequestDTO;
+import com.thalesbensi.inventory_ms.api.dtos.ProductResponseDTO;
+import com.thalesbensi.inventory_ms.domain.models.ProductModel;
+import com.thalesbensi.inventory_ms.domain.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+
+@Service
+public class ProductService {
+
+    final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductResponseDTO> findAll() {
+        return productRepository.findAll().stream()
+                .map(product -> new ProductResponseDTO(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice(),
+                        product.getQuantity(),
+                        product.getCreatedAt(),
+                        product.getUpdatedAt()
+                ))
+                .toList();
+    }
+
+
+    public ProductResponseDTO findById(@Valid Long id) {
+        ProductModel product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+      return product.toResponseDTO();
+    }
+
+    public ProductResponseDTO create(@Valid ProductRequestDTO productRequestDTO) {
+        ProductModel productModel = new ProductModel();
+        productModel.setName(productRequestDTO.name());
+        productModel.setDescription(productRequestDTO.description());
+        productModel.setPrice(productRequestDTO.price());
+        productModel.setQuantity(productRequestDTO.quantity());
+
+        ProductModel savedProduct = productRepository.save(productModel);
+        return savedProduct.toResponseDTO();
+    }
+
+    public ProductResponseDTO update(@Valid Long id, ProductRequestDTO productRequestDTO) {
+        ProductModel productModel = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        productModel.setName(productRequestDTO.name());
+        productModel.setDescription(productRequestDTO.description());
+        productModel.setPrice(productRequestDTO.price());
+        productModel.setQuantity(productRequestDTO.quantity());
+
+        ProductModel updatedProduct = productRepository.save(productModel);
+        return updatedProduct.toResponseDTO();
+    }
+
+    public void delete(@Valid Long id) {
+        ProductModel productModel = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        productRepository.delete(productModel);
+    }
+
+    public void validateStock(@Valid Long id, Integer quantity) {
+        ProductModel productModel = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        if (productModel.getQuantity() < quantity) {
+            throw new RuntimeException("Insufficient stock");
+        }
+    }
+}

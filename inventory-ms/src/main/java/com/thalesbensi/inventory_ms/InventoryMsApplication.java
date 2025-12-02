d0198a20f8e46e02625de98dd6aec751249bd9d7
thalesbensi <thalesbensi@outlook.com>
2025-12-02 17:35:19 -0300

    ProjectSetup

---
+++ inventory-ms/src/main/java/com/thalesbensi/inventory_ms/InventoryMsApplication.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.inventory_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMsApplication.class, args);
+	}
+
+}
---
+++ inventory-ms/src/test/java/com/thalesbensi/inventory_ms/InventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.inventory_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

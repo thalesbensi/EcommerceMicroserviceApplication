a23298ef6db55aa22f89806de30b42718e91588d
thalesbensi <thalesbensi@outlook.com>
2025-12-03 18:29:39 -0300

    Add SaleResponseDTO conversion method to SaleModel and apply Lombok annotations

--- sales-ms/src/main/java/com/thalesbensi/sales_ms/domain/models/SaleModel.java
+++ sales-ms/src/main/java/com/thalesbensi/sales_ms/domain/models/SaleModel.java
@@ -1,12 +1,15 @@
 package com.thalesbensi.sales_ms.domain.models;
 
+import com.thalesbensi.sales_ms.api.dtos.SaleResponseDTO;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
 @Table(name = "sales_tb")
+@Data
 public class SaleModel {
 
     @Id
@@ -25,6 +28,17 @@ public class SaleModel {
 
     @Column(name = "created_at", insertable = false, updatable = false)
     private Date createdAt;
+
+    public SaleResponseDTO toSaleResponseDTO(){
+        return new SaleResponseDTO(
+                this.id,
+                this.productId,
+                this.quantity,
+                this.unitValue,
+                this.totalValue,
+                this.createdAt
+        );
+    }
 
 
 }

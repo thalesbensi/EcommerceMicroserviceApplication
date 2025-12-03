3328bc4ae2059413be2c1f2406087251cefcae01
thalesbensi <thalesbensi@outlook.com>
2025-12-03 17:07:02 -0300

    Initialize sales microservice with Maven wrapper and basic configuration

---
+++ sales-ms/src/main/java/com/thalesbensi/sales_ms/SalesMsApplication.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.sales_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesMsApplication.class, args);
+	}
+
+}
---
+++ sales-ms/src/test/java/com/thalesbensi/sales_ms/SalesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.sales_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

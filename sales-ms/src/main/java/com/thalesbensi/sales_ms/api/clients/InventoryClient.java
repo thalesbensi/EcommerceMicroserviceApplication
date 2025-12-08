dfa650e921ca89672e0924a3d18432a585b30835
thalesbensi <thalesbensi@outlook.com>
2025-12-08 19:27:25 -0300

    Update application properties for RabbitMQ configuration

--- sales-ms/src/main/java/com/thalesbensi/sales_ms/api/clients/InventoryClient.java
+++ sales-ms/src/main/java/com/thalesbensi/sales_ms/api/clients/InventoryClient.java
@@ -4,7 +4,6 @@ import com.thalesbensi.sales_ms.api.dtos.Product.ProductResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @FeignClient(name = "inventory-ms", url = "http://localhost:8082/api/products")

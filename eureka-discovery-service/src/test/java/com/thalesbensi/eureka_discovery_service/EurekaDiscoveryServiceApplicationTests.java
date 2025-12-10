6847abde6a9d902ef6e7ad462fd6e1e387730637
thalesbensi <thalesbensi@outlook.com>
2025-12-10 01:16:10 -0300

    Initialize Eureka Discovery Service with Maven setup and basic configuration

---
+++ eureka-discovery-service/src/test/java/com/thalesbensi/eureka_discovery_service/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.eureka_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

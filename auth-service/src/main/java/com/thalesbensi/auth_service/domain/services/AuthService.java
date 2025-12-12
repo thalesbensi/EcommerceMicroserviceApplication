dc5148ec4d37ad3870bac36dea10322be4a872ec
thalesbensi <thalesbensi@outlook.com>
2025-12-11 22:53:05 -0300

    Remove unused JwtService dependency from AuthService

--- auth-service/src/main/java/com/thalesbensi/auth_service/domain/services/AuthService.java
+++ auth-service/src/main/java/com/thalesbensi/auth_service/domain/services/AuthService.java
@@ -13,12 +13,10 @@ public class AuthService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
 
     public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
     }
 
     @Override

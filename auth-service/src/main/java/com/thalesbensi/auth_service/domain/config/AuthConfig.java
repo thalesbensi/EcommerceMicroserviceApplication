1fd50dc0221928cc08ced5b0c01da1c9646af87e
thalesbensi <thalesbensi@outlook.com>
2025-12-11 22:50:37 -0300

    Update AuthConfig to restrict inventory API access to ADMIN role

--- auth-service/src/main/java/com/thalesbensi/auth_service/domain/config/AuthConfig.java
+++ auth-service/src/main/java/com/thalesbensi/auth_service/domain/config/AuthConfig.java
@@ -2,6 +2,7 @@ package com.thalesbensi.auth_service.domain.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,8 +30,9 @@ public class AuthConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/auth/**").permitAll()
-
-
+                        .requestMatchers(HttpMethod.POST,"/inventory-api").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/inventory-api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/inventory-api/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)

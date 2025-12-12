cc850de85a13179c6b3c119fe1aa87289ad2544b
thalesbensi <thalesbensi@outlook.com>
2025-12-11 22:43:11 -0300

    Add AuthService for user authentication and details loading

---
+++ auth-service/src/main/java/com/thalesbensi/auth_service/domain/services/AuthService.java
@@ -0,0 +1,28 @@
+package com.thalesbensi.auth_service.domain.services;
+
+import com.thalesbensi.auth_service.domain.models.user.UserModel;
+import com.thalesbensi.auth_service.domain.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.findByLogin(login);
+    }
+}

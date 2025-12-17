2878e3aea26bfbde7844f17e98a0d7ac316c786b
thalesbensi <thalesbensi@outlook.com>
2025-12-17 14:09:31 -0300

    Add validation annotations to ProductRequestDTO fields

--- inventory-ms/src/main/java/com/thalesbensi/inventory_ms/api/dtos/productDtos/ProductRequestDTO.java
+++ inventory-ms/src/main/java/com/thalesbensi/inventory_ms/api/dtos/productDtos/ProductRequestDTO.java
@@ -1,5 +1,21 @@
 package com.thalesbensi.inventory_ms.api.dtos.productDtos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
-public record ProductRequestDTO(String name, String description, BigDecimal price, Integer quantity){}
+public record ProductRequestDTO(
+        @NotNull(message = "The Product Name cannot be null")
+        String name,
+
+        @NotNull(message = "The Product Description cannot be null")
+        String description,
+
+        @Positive(message = "The quantity must be greater than zero")
+        @NotNull(message = "The Product Price cannot be null")
+        BigDecimal price,
+
+        @Positive(message = "The quantity must be greater than zero")
+        @NotNull(message = "The Product Quantity cannot be null")
+        Integer quantity){}
